45a975dcd14152e387b075f1674e6fce793b5d92
Varvara Mamatsiuk <matyush2001@gmail.com>
2022-08-29 12:05:33 +0300

    Injected opportunity to update task and to delete tasks.

--- src/main/java/krekerok/code/krekerok_task_tracker/api/controllers/TaskController.java
+++ src/main/java/krekerok/code/krekerok_task_tracker/api/controllers/TaskController.java
@@ -1,16 +1,15 @@
 package krekerok.code.krekerok_task_tracker.api.controllers;
 
 import krekerok.code.krekerok_task_tracker.api.controllers.helpers.ControllerHelper;
+import krekerok.code.krekerok_task_tracker.api.dto.AskDto;
+import krekerok.code.krekerok_task_tracker.api.dto.ProjectDto;
 import krekerok.code.krekerok_task_tracker.api.dto.TaskDto;
-import krekerok.code.krekerok_task_tracker.api.dto.TaskStateDto;
 import krekerok.code.krekerok_task_tracker.api.exceptions.BadRequestException;
 import krekerok.code.krekerok_task_tracker.api.exceptions.NotFoundException;
 import krekerok.code.krekerok_task_tracker.api.factories.TaskDtoFactory;
-import krekerok.code.krekerok_task_tracker.api.factories.TaskStateDtoFactory;
 import krekerok.code.krekerok_task_tracker.store.entities.ProjectEntity;
 import krekerok.code.krekerok_task_tracker.store.entities.TaskEntity;
 import krekerok.code.krekerok_task_tracker.store.entities.TaskStateEntity;
-import krekerok.code.krekerok_task_tracker.store.repositories.ProjectRepository;
 import krekerok.code.krekerok_task_tracker.store.repositories.TaskRepository;
 import krekerok.code.krekerok_task_tracker.store.repositories.TaskStateRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -30,6 +30,7 @@ public class TaskController {
     private final TaskStateRepository taskStateRepository;
     private final TaskDtoFactory taskDtoFactory;
 
+    private final ControllerHelper controllerHelper;
 
     public static final String GET_TASKS = "/api/task-states/{task_state_id}/tasks";
     public static final String CREATE_TASK = "/api/task-states/{task_state_id}/tasks";
@@ -39,7 +40,7 @@ public class TaskController {
     @GetMapping(GET_TASKS)
     public List<TaskDto> getTasks(@PathVariable(value = "task_state_id") Long taskStateId) {
 
-        TaskStateEntity taskState = getTaskStateOrThrowExeption(taskStateId);
+        TaskStateEntity taskState = controllerHelper.getTaskStateOrThrowException(taskStateId);
 
         return taskState
                 .getTasks()
@@ -59,7 +60,7 @@ public class TaskController {
             throw new BadRequestException("Information about task can't be empty.");
         }
 
-        TaskStateEntity taskState = getTaskStateOrThrowExeption(taskStateId);
+        TaskStateEntity taskState = controllerHelper.getTaskStateOrThrowException(taskStateId);
 
         for (TaskEntity task: taskState.getTasks()){
             if (task.getName().equalsIgnoreCase(taskName)){
@@ -81,13 +82,49 @@ public class TaskController {
         return taskDtoFactory.makeTaskDto(savedTask);
     }
 
-    private TaskStateEntity getTaskStateOrThrowExeption(Long taskStateId) {
-        return taskStateRepository.findById(taskStateId)
-                .orElseThrow(() ->
-                        new NotFoundException(
-                                String.format("Task state with \"%s\" doesn't exist.", taskStateId)
-                        )
-                );
+
+
+
+    @PatchMapping(UPDATE_TASK)
+    public TaskDto updateTask(
+            @PathVariable(value = "task_id") Long taskId,
+            @RequestParam(value = "task_name", required = false) Optional<String> optionalTaskName,
+            @RequestParam(value = "task_description", required = false) Optional<String> optionalTaskDescription
+    ) {
+
+
+        optionalTaskName = optionalTaskName.filter(taskName -> !taskName.trim().isEmpty());
+        optionalTaskDescription = optionalTaskDescription.filter(taskDescription -> !taskDescription.trim().isEmpty());
+
+
+        TaskEntity task = controllerHelper.getTaskOrThrowException(taskId);
+
+        if (!optionalTaskName.isPresent() && !optionalTaskDescription.isPresent()){
+            throw new BadRequestException("Project name can't be empty.");
+        }
+
+        optionalTaskName
+                .ifPresent(taskName -> {
+                    task.setName(taskName);
+                });
+
+        optionalTaskDescription
+                .ifPresent(taskDescription -> {
+                    task.setDescription(taskDescription);
+                });
+
+        final TaskEntity savedTask = taskRepository.saveAndFlush(task);
+
+        return taskDtoFactory.makeTaskDto(savedTask);
+    }
+
+
+    @DeleteMapping(DELETE_TASK)
+    public AskDto deleteTask(@PathVariable("task_id") Long taskId){
+        controllerHelper.getTaskOrThrowException(taskId);
+
+        taskRepository.deleteById(taskId);
+        return AskDto.makeDefault(true);
     }
 
 }
--- src/main/java/krekerok/code/krekerok_task_tracker/api/controllers/helpers/ControllerHelper.java
+++ src/main/java/krekerok/code/krekerok_task_tracker/api/controllers/helpers/ControllerHelper.java
@@ -2,7 +2,11 @@ package krekerok.code.krekerok_task_tracker.api.controllers.helpers;
 
 import krekerok.code.krekerok_task_tracker.api.exceptions.NotFoundException;
 import krekerok.code.krekerok_task_tracker.store.entities.ProjectEntity;
+import krekerok.code.krekerok_task_tracker.store.entities.TaskEntity;
+import krekerok.code.krekerok_task_tracker.store.entities.TaskStateEntity;
 import krekerok.code.krekerok_task_tracker.store.repositories.ProjectRepository;
+import krekerok.code.krekerok_task_tracker.store.repositories.TaskRepository;
+import krekerok.code.krekerok_task_tracker.store.repositories.TaskStateRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +18,8 @@ import javax.transaction.Transactional;
 public class ControllerHelper {
 
     private final ProjectRepository projectRepository;
+    private final TaskStateRepository taskStateRepository;
+    private final TaskRepository taskRepository;
 
     public ProjectEntity getProjectOrThrowException(Long projectId) {
         return projectRepository
@@ -21,6 +27,24 @@ public class ControllerHelper {
                 .orElseThrow(() ->
                         new NotFoundException(
                                 String.format("Project with \"%s\" doesn't exist.", projectId)
+                        )
+                );
+    }
+
+   public TaskStateEntity getTaskStateOrThrowException(Long taskStateId) {
+        return taskStateRepository.findById(taskStateId)
+                .orElseThrow(() ->
+                        new NotFoundException(
+                                String.format("Task state with \"%s\" doesn't exist.", taskStateId)
+                        )
+                );
+    }
+
+    public TaskEntity getTaskOrThrowException(Long taskId) {
+        return taskRepository.findById(taskId)
+                .orElseThrow(() ->
+                        new NotFoundException(
+                                String.format("Task with \"%s\" doesn't exist.", taskId)
                         )
                 );
     }
